ea868daf8fdaa8035054258eca5c1560664da39b
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2018-03-24 22:05:14 -0500

    Original version

---
+++ whelpV1_0.js
@@ -0,0 +1,383 @@
+// Introduction
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const botGuild = require("./botguild.json"); //Bot Guild IDs
+const dracGuild = require("./dracarg.json"); //Draconian Argentum Guild IDs
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+// Constants
+// Bot related
+const prefix = config.prefix;
+
+// Emojis
+const reactEmoji = "423264671030837303";
+
+const caseList = [
+  "!help",                // 0
+  "!ayuda",               // 1
+  "!list",                // 2
+  "!lista",               // 3
+  "!emoji",               // 4
+  "!roleID",              // 5
+  "!gamesoffered",        // 6
+  "!ping",                // 7
+  "!me",                  // 8
+  "!ARK",                 // 9
+  "!BF",                  // 10
+  "!ESO",                 // 11
+  "!Haven&Hearth",        // 12
+  "!LOL",                 // 13
+  "!Minecraft",           // 14
+  "!PUBG",                // 15
+  "!Terraria"             // 16
+];
+
+
+// When guild members are added
+client.on("guildMemberAdd", (addedMember) =>{
+  var userTag = addedMember.user;
+  var displayedName = addedMember.displayName;
+  var guildAdded = addedMember.guild.id;
+
+  if (guildAdded == dracGuild.guildID){
+    var msg = "Welcome to Draconian Argentum! I am Whelp and I help you get around on the server. ";
+    client.guilds.get(dracGuild.guildID).channels.get(dracGuild.welcomeChat).send("Hello "+displayedName+"! ");
+    client.guilds.get(dracGuild.guildID).channels.get(dracGuild.botReport).send(userTag+" You can send me commands in this channel. For more information, send !help");
+  }else{
+    client.guilds.get(botGuild.guildID).channels.get(botGuild.genChat).send("hello");
+    client.guilds.get(botGuild.guildID).channels.get(botGuild.genChat).send(userTag+" You can send me commands in this channel. For more information, send !help");
+  }
+});
+
+// When a person is banned
+client.on("guildBanAdd", (guildin, userin) => {
+    var bannedUser = userin.id;
+    var banGuild = guildin.id;
+    const JubJubID = 218400607860424704;
+
+    if (bannedUser == JubJubID){
+      client.guilds.get(dracGuild.guildID).channels.get(dracGuild.genChat).send("LMAO JUBJUB GOT FREAKIN BANNED!");
+    }
+});
+
+client.on("message", (message) => {
+  var channelType = message.channel.type;
+  var acceptedType = "dm";
+  if (!message.content.startsWith(prefix) || message.author.bot){
+   return;
+  }
+  if (channelType == acceptedType) {
+    return;
+  }
+  var contentsMess = message.content.split(' ');
+  var cmd = contentsMess[0];
+  var name = message.author;
+  var nameCheck = message.member.nickname;
+  var roleList = message.member.roles;
+  var roleCheck = roleList.map(r => r.id);
+  var removeTheRole = 0;
+  var triggers = 1;
+
+  // if someone tries a command of their nickname
+  var messageCheck = message.content.substring(1);
+  if (messageCheck == nameCheck) {
+    message.channel.send("All hail "+nameCheck+"!");
+  }
+
+  switch(cmd) {
+    case caseList[0]: // !help
+      message.channel.send(
+        "Hello! I'm Whelp. I'm a bot created to help with " +
+        "moderation within this chat. " +
+        "You can send me commands to join the different "+
+        "chats and channels according to the games you play. "+
+        "My commands follow a format of !<game>. "+
+        "A couple examples: \n"+
+        "!ESO \n!ARK \n!LOL\n"+
+        "To have a list of my commands sent to you, type '!list'."
+      );
+      break;
+
+    case caseList[1]: // !ayuda
+      message.channel.send(
+        "Olá! Eu sou Whelp. Sou um bot criado para ajudar " +
+        " com a moderação deste chat." +
+        "Você pode me enviar comandos para se juntar aos diferentes " +
+        "chats e canais de acordo com os jogos que você joga." +
+        "Meus comandos seguem o seguinte formato: !<jogo>." +
+        "Alguns exemplos: \n" +
+        "!ESO \n!ARK \n !LOL\n" +
+        "Para ver uma lista de todos os meus comandos, digite '!lista'."
+      );
+      break;
+
+  case caseList[2]: // !list
+    message.author.createDM();
+    var commandList = "";
+    for (var i = 0; i < caseList.length; i++) {
+      if (i == 4 || i == 5) {
+      }else {
+        commandList += caseList[i] + "\n";
+      }
+    }
+    message.author.send("This is the list of my commands. \n"+commandList);
+    break;
+
+  case caseList[3]: // !lista
+    message.author.createDM();
+    var commandList = "";
+    for (var i = 0; i < caseList.length; i++) {
+      if (i == 4 || i == 5) {
+      }else {
+        commandList += caseList[i] + "\n";
+      }
+    }
+    message.author.send("Essa é a lista de meus comandos. \n"+commandList);
+    break;
+
+  case caseList[4]: // !emoji
+    var owner = "215225483942428672";
+    var name2 = message.author.id;
+    if (name2 == owner){
+      var emojis = message.guild.emojis;
+      console.log(emojis);
+    }
+    break;
+
+  case caseList[5]: // !roleID
+    var idspls = roleList.map(r => r.id); // isolates the role ids
+    var absrolelist = message.guild.roles;
+    console.log(absrolelist);
+    break;
+
+  case caseList[6]: // !gamesoffered
+    message.channel.send("Game Categories Currently Offered:\n"+
+    "Ark [!ARK]\nBattlefield [!BF]\nElder Scrolls Online [!ESO]"+
+    "\nHaven & Hearth [!Haven&Hearth]"+
+    "\nLeague of Legends [!LOL]\nMinecraft [!Minecraft]"+
+    "\nPLAYERUNKNOWN's Battlegrounds [!PUBG]\nTerraria [!Terraria]"
+    );
+    break;
+
+  case caseList[7]: // !ping
+    message.channel.send("NO! NO PONG!");
+    break;
+
+  case caseList[8]: // !me
+    var owner = "215225483942428672";
+    var name2 = message.author.id;
+    if (name2 == owner){
+      message.channel.send("You are "+name+", silly! You're a piece of shit!");
+    } else {
+      message.channel.send("You are "+name+"!");
+    }
+    break;
+
+  case caseList[9]: // !ARK
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.ARK);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.ARK);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.roles.ARK);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  case caseList[10]: // !BF
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.BF);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.BF);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.roles.BF);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  case caseList[11]: // !ESO
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.ESO);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.ESO);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.roles.ESO);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  case caseList[12]: // !Haven&Hearth
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.Haven);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.Haven);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.roles.Haven);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  case caseList[13]: // !LOL
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.LOL);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.LOL);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.roles.LOL);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  case caseList[14]: // !Minecraft
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.Minecraft);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.Minecraft);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.roles.Minecraft);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  case caseList[15]: // !PUBG
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.PUBG);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.PUBG);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.roles.PUBG);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  case caseList[16]: // !Terraria
+    for (i = 0; i<roleCheck.length;i++){
+      var x = new Boolean(roleCheck[i]==dracGuild.roles.Terraria);
+      // if this is true, you have the role
+
+      if (x == true) {
+        message.member.removeRole(dracGuild.roles.Terraria);
+        removeTheRole = 1;
+        break; //remove the role and break out of the for loop
+      }else{
+        //do nothing and continue
+      }
+    } // out of the for loop
+
+    if (removeTheRole){
+      message.react(reactEmoji);
+      break; //role removed, break out of the case
+    }else if(removeTheRole ==0){
+      message.member.addRole(dracGuild.role.Terraria);
+      message.react(reactEmoji);
+      break;//role added, break out of the case
+    }
+
+  default:
+    triggers = 0;
+    break;
+  } // ends switch
+
+  var reporttxt = " asked me ";
+  var tagName = name.username;
+  var badReportTxt = " tried to use "
+
+  //cmd
+  if (triggers){
+    client.guilds.get(botGuild.guildID).channels.get(botGuild.botReport).send(tagName+"/"+nameCheck+reporttxt+cmd);
+  }else{
+    client.guilds.get(botGuild.guildID).channels.get(botGuild.botReport).send(tagName+"/"+nameCheck+badReportTxt+cmd);
+  }
+
+});
+
+// to log in the bot
+client.login(config.token);
