0979b5cca72cbe83e372d5ac1cadcb3620356472
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2018-03-26 17:30:38 -0500

    commandlineexecutor

--- commandlineexecutor.js
+++ commandlineexecutor.js
@@ -1,7 +1,7 @@
 // gonna try to send ping website to powershell and then read response
 
 const Discord = require("discord.js");
-const exec = require('child_process');
+const { exec } = require('child_process');
 const client = new Discord.Client();
 const config = require("./config.json");
 const botGuild = require("./botguild.json"); //Bot Guild IDs
@@ -13,10 +13,28 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
 
+  if (message.content.startsWith("pinger")){
+    exec('ping game.havenandhearth.com', (err,stdout,stderr) => {
+      if (err){
+        return;
+      }
+      console.log('stdout: ${stdout}',stdout);
+      // gotta parse through the message
+      var pos = stdout.indexOf("Lost"); //number of index where Lost will start
+      var importantPos = pos + 7;
+      var numOfLosses = stdout.substr(importantPos,1);
 
-exec('ping game.havenandhearth.com', (err,stdout,stderr) => {
-  if (err){
-    return;
+      if (numOfLosses == "0"){
+        message.channel.send("The game server seems to be responding.")
+      } else if (numOfLosses == "4"){
+        message.channel.send("The game server appears to be down.")
+      } else {
+        message.channel.send("The game server may or may not be down.")
+      }
+
+    });
   }
-  console.log('stdout: ${stdout}');
 });
+
+// to log in the bot
+client.login(config.token);
